660191b1da2114450e9141ead3484726313e0ab6
Juliano <juliano.buss@gmail.com>
2022-02-15 10:44:18 -0300

    Add files via upload

---
+++ Curso em Vídeo - Curso JS/Aula15/EX01.js
@@ -0,0 +1,11 @@
+var num = [33]
+console.log(num)
+console.log(num.length)
+
+num[0] = 5
+num.push(2)
+num[2] = 3
+num.push(4)
+console.log(num)
+console.log(num.length)
+console.log(num.sort())
---
+++ Curso em Vídeo - Curso JS/Aula15/Ex02.js
@@ -0,0 +1,6 @@
+var Nums = []
+
+for(let i=0 ; i<5; i++){
+    Nums[i] = i + 3
+    console.log(`i: ${i} number: ${Nums[i]}`)
+}
